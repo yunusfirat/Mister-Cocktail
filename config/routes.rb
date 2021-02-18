6641781921b514b9cb34fc5c0c8b1684c4e4841f
yunusfirat <yunusfirat44@gmail.com>
2021-02-18 15:36:57 +0000

    routes is written

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :coaktails,:description, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:create] 
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :index, :delete]
   end
-
-
 end
